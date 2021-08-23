37fb85bd213184861859b4223120d678ad564f5d
Robyn Beck <deft.or.die@gmail.com>
2021-08-23 16:36:24 -0700

    Added giphy api data

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect } from "react";
-import axios from "axios";
+import { useEffect } from 'react'
+import axios from 'axios'
+import SearchBar from './components/SearchBar'
 
 function App() {
   useEffect(() => {
     axios
-      .get("/api")
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  }, []);
+      .get('/api')
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <SearchBar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,9 +1,23 @@
 const router = require("express").Router();
 require("dotenv").config();
+const axios = require('axios')
 
-router.get("/api", (req, res) => {
-  console.log(process.env.API_KEY);
-  res.json({ msg: "success" });
-});
+router.get('/api', async (req, res) => {
+  axios
+    .get(`http://api.giphy.com/v1/gifs/trending?limit=30&api_key=${process.env.API_KEY}`)
+    .then(response => {
+      console.log(response.data)
+      res.json(response.data)
+    })
+    .catch(err => {
+      res.json(err)
+    })
+})
+
+router.get('/trending', (req, res) => {
+  console.log('Successfully hit Internal API')
+})
+
+
 
 module.exports = router;
