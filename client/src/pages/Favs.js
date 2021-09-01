0ae93f7c475d4976a8b654880bbe558ac18e8dc0
Robyn Beck <deft.or.die@gmail.com>
2021-09-01 13:17:17 -0700

    added stling for favs page

--- client/src/pages/Favs.js
+++ client/src/pages/Favs.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { FavsGallery, FavsHeader } from '../components/Favs/index'
 
 const Container = styled.div`
   width: 60vw;
   height: 100dvh;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
 `
 
 const Favorites = () => {
