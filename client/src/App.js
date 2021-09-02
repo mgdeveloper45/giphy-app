358dad72e10457b9ace430a8536cba7e3a2a8b26
MarkGee45 <moel55.mg@gmail.com>
2021-09-02 10:55:35 -0700

    clear input and refactor component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
 import { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import axios from "axios";
-
 
 import Button from "./components/SearchBar/Button.js";
 import Input from "./components/SearchBar/Input.js";
@@ -16,27 +11,29 @@ import Nav from "./components/Nav.js";
 import Home from "./pages/Home.js";
 import SearchResults from "./components/SearchResults.js";
 import SearchGifsPage from "./pages/SearchGifsPage.js";
-import Favorites from './pages/Favs'
+import Favorites from "./pages/Favs";
 import Random from "./pages/Random.js";
 
 function App() {
   const [value, setValue] = useState("");
   const [searchGifs, setSearchGifs] = useState([]);
   const [toggle, setToggle] = useState(false);
-  const [currentSearch, setCurrentSearch] = useState("")
+  const [currentSearch, setCurrentSearch] = useState("");
 
   const onChange = (e) => {
     setValue(e.target.value);
   };
-  
+
   const searchForGifs = async (search) => {
     try {
-      const searchResults = await axios.get(`/api/search?q=${search}`)
-      setSearchGifs(searchResults.data)
+      const searchResults = await axios.get(`/api/search?q=${search}`);
+      setSearchGifs(searchResults.data);
+      document.querySelector("#searchInput").value = "";
+      setValue("");
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   return (
     <AppContainer>
@@ -46,29 +43,30 @@ function App() {
           <Input onChange={onChange} />
           <Button
             onClick={() => {
-              setToggle(true)
+              setToggle(true);
               searchForGifs(value);
-              setCurrentSearch(value)
+              setCurrentSearch(value);
             }}
-            type='submit'
+            type="submit"
           />
         </Search>
+
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/searchpage">
             <SearchGifsPage
-            currentSearch={currentSearch} 
-            toggle={toggle}
-            value={value} 
-            searchGifs={searchGifs}
+              currentSearch={currentSearch}
+              toggle={toggle}
+              value={value}
+              searchGifs={searchGifs}
             />
           </Route>
-          <Route path="/favs" component={Favorites}/>
-          <Route path="/random" component={Random}/>
+          <Route path="/favs" component={Favorites} />
+          <Route path="/random" component={Random} />
         </Switch>
       </Router>
     </AppContainer>
-  )
+  );
 }
 
 export default App;
--- client/src/components/SearchBar/Input.js
+++ client/src/components/SearchBar/Input.js
@@ -1,24 +1,12 @@
-import React from 'react'
 import styled from "styled-components";
 
-const InputContainer = styled.input`
+const Input = styled.input.attrs({
+  id: "searchInput",
+  type: "text",
+  placeholder: "Search for GIFS",
+})`
   width: 90%;
-  height:2rem;
-
+  height: 2rem;
 `;
 
-
-
-const Input = (props) => {
-  return (
-    <InputContainer
-      type="text"
-      onChange={props.onChange}
-      placeholder='Search for GIFS'
-    />
-  );
-}
-
-
-
-export default Input
+export default Input;
